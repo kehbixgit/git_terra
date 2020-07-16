7c395061abd4fcab586123e62f94587864cb48d6
kehbixgit <kehbix@gmail.com>
2020-07-16 15:26:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "aws_instance" "default" {
 }
 
 # Create Security Group for EC2
-resource "aws_security_group" "default" {
-  name = "terraform-default-sg"
+resource "aws_security_group" "DevOps" {
+  name = "DevOps"
 
   ingress {
     from_port   = 80
